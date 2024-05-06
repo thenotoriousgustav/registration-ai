4889babbcbaa1bc47ef11deabbedf2e79679ffc8
nicholasali <nicholasali.888@gmail.com>
2024-05-06 16:21:12 +0700

    add form

--- src/components/pilih-ujian/card-ujian.tsx
+++ src/components/pilih-ujian/card-ujian.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -6,34 +6,36 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import Link from 'next/link';
-import Image from 'next/image';
+} from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import { DialogUjian } from "./dialog-ujian";
 
 export default function CardUjian({ redirect }: { redirect: string }) {
   return (
-    <Card className='w-[350px] hover:-translate-y-2  transform transition-all '>
-      <CardHeader className='p-0'>
+    <Card className="w-[350px] hover:-translate-y-2  transform transition-all ">
+      <CardHeader className="p-0">
         <Image
-          src='/img/img-1.jpeg'
-          alt='asdas'
-          className='w-full rounded-md object-cover h-46'
-          width='0'
-          height='0'
-          sizes='100vw'
+          src="/img/img-1.jpeg"
+          alt="asdas"
+          className="w-full rounded-md object-cover h-46"
+          width="0"
+          height="0"
+          sizes="100vw"
         />
       </CardHeader>
-      <CardContent className='pt-6'>
-        <CardTitle className='text-xl font-semibold'>Lorem, ipsum.</CardTitle>
-        <CardDescription className='mt-2'>
+      <CardContent className="pt-6">
+        <CardTitle className="text-xl font-semibold">Lorem, ipsum.</CardTitle>
+        <CardDescription className="mt-2">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet corporis
           illo enim aspernatur odio earum.
         </CardDescription>
       </CardContent>
-      <CardFooter className='mt-2'>
-        <Button asChild>
+      <CardFooter className="mt-2">
+        {/* <Button asChild>
           <Link href={redirect}>Daftar</Link>
-        </Button>
+        </Button> */}
+        <DialogUjian />
       </CardFooter>
     </Card>
   );
--- src/components/pilih-ujian/dialog-ujian.tsx
+++ src/components/pilih-ujian/dialog-ujian.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -8,47 +9,55 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
-export function DialogDemo() {
+export function DialogUjian() {
+  const [isFinish, setIsFinish] = useState(true);
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
+        <Button variant="outline">Lihat</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+        {isFinish ? (
+          <p>Anda sudah terdaftar dan sudah lulus</p>
+        ) : (
+          <>
+            <DialogHeader>
+              <DialogTitle>Daftar Ujian</DialogTitle>
+              <DialogDescription>
+                Silahkan masukkan data diri anda sebelum ujian dimulai.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Nama
+                </Label>
+                <Input
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Umur
+                </Label>
+                <Input
+                  id="username"
+                  defaultValue="@peduarte"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
