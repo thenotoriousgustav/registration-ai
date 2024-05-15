cd717cf5aad8fee8b041e9411fcaac0c1e4b2b60
thenotoriousgustav <rhezagustam@gmail.com>
2024-05-15 13:13:10 +0700

    fix models

--- src/components/face-verification-comp.tsx
+++ src/components/face-verification-comp.tsx
@@ -24,12 +24,12 @@ export default function FaceVerificationComp() {
     // load models
     const loadModels = async () => {
       await Promise.all([
-        faceapi.nets.ssdMobilenetv1.loadFromUri("models"),
-        faceapi.nets.tinyFaceDetector.loadFromUri("models"),
-        faceapi.nets.faceLandmark68Net.loadFromUri("models"),
-        faceapi.nets.faceRecognitionNet.loadFromUri("models"),
-        faceapi.nets.faceExpressionNet.loadFromUri("models"),
-        faceapi.nets.ageGenderNet.loadFromUri("models"),
+        faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+        faceapi.nets.ageGenderNet.loadFromUri("/models"),
       ]);
     };
 
